24239990d68c4dacbb1b4a1741c4228d121ccea2
Renuka Fernando <renukapiyumal@gmail.com>
2025-05-14 18:16:31 +0530

    Add initial implementation of Envoy External Processor with Docker setup and example resources

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # envoy-ext-proc-body-streaming
+
 POC on using Envoy External Processor (ext proc) to process body in streaming mode.
+
+Please update the netty-echo image in [docker-compose.yaml](docker-compose.yaml) to the image that matches your local environment architecture.
+
+## Run Ext Proc Service
+
+```sh
+cd external_processor;
+go run main.go
+```
+
+## Check Memory Consumption
+
+```sh
+docker stats
+```
+
+## Test the Ext Proc Service
+
+```sh
+for i in {1..10}; do
+    curl --location 'http://localhost:18080/pets/myPetId123/history' -vvv \
+    --header 'Content-Type: video/mp4' \
+    --data-binary '@./resources/i_want_it_that_way.mp4' -o "./temp/response-${i}.mp4" &;
+done
+```
---
+++ external_processor/main.go
@@ -0,0 +1,271 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"math/rand"
+	"net"
+	"os"
+	"time"
+
+	ext_proc_v3 "github.com/envoyproxy/go-control-plane/envoy/service/ext_proc/v3"
+	pb "github.com/envoyproxy/go-control-plane/envoy/service/ext_proc/v3"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"google.golang.org/grpc"
+)
+
+var write_data_to_file *bool
+
+var _ ext_proc_v3.ExternalProcessorServer = &server{}
+
+var new_req_body []byte
+var new_req_body_chunks [][]byte
+
+var new_resp_body []byte
+var new_resp_body_chunks [][]byte
+
+const chunkSize = 1 << 20 // 1 MiB = 1048576 bytes
+
+func splitIntoChunks(data []byte) [][]byte {
+	var chunks [][]byte
+	for len(data) > 0 {
+		end := chunkSize
+		if len(data) < chunkSize {
+			end = len(data)
+		}
+		chunks = append(chunks, data[:end])
+		data = data[end:]
+	}
+	return chunks
+}
+
+type server struct {
+}
+
+// Process implements ext_procv3.ExternalProcessorServer.
+func (s *server) Process(processServer ext_proc_v3.ExternalProcessor_ProcessServer) error {
+	ctx := processServer.Context()
+	rnd := rand.Int()
+
+	req_payload_file := &os.File{}
+	var err error
+	if *write_data_to_file {
+		req_payload_filename := fmt.Sprintf("/Users/renuka/Downloads/ext_proc_test/temp/%d-request_payload.mp4", rnd)
+		req_payload_file, err = os.OpenFile(req_payload_filename, os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			fmt.Println("Error opening or creating file:", err)
+		}
+		defer req_payload_file.Close()
+	}
+
+	resp_payload_file := &os.File{}
+	if *write_data_to_file {
+		resp_payload_filename := fmt.Sprintf("/Users/renuka/Downloads/ext_proc_test/temp/%d-response_body.mp4", rnd)
+		resp_payload_file, err = os.OpenFile(resp_payload_filename, os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			fmt.Println("Error opening or creating file:", err)
+		}
+		defer resp_payload_file.Close()
+	}
+
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		default:
+		}
+		req, err := processServer.Recv()
+		if err == io.EOF {
+			log.Info().Msg("EOF ******************************************")
+			return nil
+		}
+		if err != nil {
+			// log.Error().Err(err).Msg("Error receiving stream request")
+			// return status.Errorf(codes.Unknown, "cannot receive stream request: %v", err)
+			log.Info().Err(err).Msg("Stream completed")
+			return nil
+		}
+
+		log.Info().Msgf("Processing Request : %d", rnd)
+		time.Sleep(1 * time.Microsecond)
+		// route_name := req.MetadataContext.FilterMetadata["envoy.filters.http.ext_proc"].Fields["meta.route_name"].GetStringValue()
+
+		switch value := req.Request.(type) {
+
+		case *pb.ProcessingRequest_RequestHeaders:
+			log.Info().Msgf("******** Processing Request Headers ********* %v", rnd)
+			resp := &pb.ProcessingResponse{
+				Response: &pb.ProcessingResponse_RequestHeaders{},
+			}
+
+			if err := processServer.Send(resp); err != nil {
+				log.Error().Err(err).Msg("Error sending response")
+			}
+
+		case *pb.ProcessingRequest_RequestBody:
+			log.Info().Msgf("******** Processing Request Body ********* %v", rnd)
+			body := value.RequestBody.Body
+
+			if *write_data_to_file {
+				_, err = req_payload_file.Write(body)
+				if err != nil {
+					log.Error().Err(err).Msg("Error writing to file")
+				}
+			}
+
+			if value.RequestBody.EndOfStream {
+				log.Info().Msg("Request Body EOF ****************************************************************************************")
+				log.Info().Msgf("Updating file with new content. Length: %v", len(new_req_body))
+
+				for i, _ := range new_req_body_chunks {
+
+					// Response for FULL_DUPLEX_STREAMED
+					resp := &pb.ProcessingResponse{
+						Response: &pb.ProcessingResponse_RequestBody{
+							RequestBody: &pb.BodyResponse{
+								Response: &pb.CommonResponse{
+									BodyMutation: &pb.BodyMutation{
+										Mutation: &pb.BodyMutation_StreamedResponse{
+											StreamedResponse: &pb.StreamedBodyResponse{
+												Body:        new_req_body_chunks[i],
+												EndOfStream: i == len(new_req_body_chunks)-1,
+											},
+										},
+									},
+								},
+							},
+						},
+					}
+
+					if err := processServer.Send(resp); err != nil {
+						log.Error().Err(err).Msg("Error sending response")
+					}
+				}
+			}
+
+			// // Response for STREAMED
+			// resp := &pb.ProcessingResponse{
+			// 	Response: &pb.ProcessingResponse_RequestBody{
+			// 		RequestBody: &pb.BodyResponse{
+			// 			Response: &pb.CommonResponse{
+			// 				BodyMutation: &pb.BodyMutation{
+			// 					Mutation: &pb.BodyMutation_Body{
+			// 						Body: []byte{},
+			// 					},
+			// 				},
+			// 			},
+			// 		},
+			// 	},
+			// }
+			// if err := processServer.Send(resp); err != nil {
+			// 	log.Error().Err(err).Msg("Error sending response")
+			// }
+
+		case *pb.ProcessingRequest_ResponseHeaders:
+			log.Info().Msgf("******** Processing Response Headers ********* %v", rnd)
+
+			resp := &pb.ProcessingResponse{
+				Response: &pb.ProcessingResponse_ResponseHeaders{},
+			}
+
+			if err := processServer.Send(resp); err != nil {
+				log.Error().Err(err).Msg("Error sending response")
+			}
+
+		case *pb.ProcessingRequest_ResponseBody:
+			log.Info().Msgf("******** Processing Response Body ********* %v", rnd)
+
+			body := value.ResponseBody.Body
+			if *write_data_to_file {
+				_, err = resp_payload_file.Write(body)
+				if err != nil {
+					log.Error().Err(err).Msg("Error writing to file")
+				}
+			}
+
+			if value.ResponseBody.EndOfStream {
+				log.Info().Msg("Response Body EOF ****************************************************************************************")
+				log.Info().Msgf("Updating file with new content. Length: %v", len(new_resp_body))
+
+				for i, _ := range new_resp_body_chunks {
+					resp := &pb.ProcessingResponse{
+						Response: &pb.ProcessingResponse_ResponseBody{
+							ResponseBody: &pb.BodyResponse{
+								Response: &pb.CommonResponse{
+									BodyMutation: &pb.BodyMutation{
+										Mutation: &pb.BodyMutation_StreamedResponse{
+											StreamedResponse: &pb.StreamedBodyResponse{
+												Body:        new_resp_body_chunks[i],
+												EndOfStream: i == len(new_resp_body_chunks)-1,
+											},
+										},
+									},
+								},
+							},
+						},
+					}
+
+					if err := processServer.Send(resp); err != nil {
+						log.Error().Err(err).Msg("Error sending response")
+					}
+				}
+			}
+
+			// // Response for STREAMED
+			// resp := &pb.ProcessingResponse{
+			// 	Response: &pb.ProcessingResponse_ResponseBody{
+			// 		ResponseBody: &pb.BodyResponse{
+			// 			Response: &pb.CommonResponse{
+			// 				BodyMutation: &pb.BodyMutation{
+			// 					Mutation: &pb.BodyMutation_Body{
+			// 						Body: []byte{},
+			// 					},
+			// 				},
+			// 			},
+			// 		},
+			// 	},
+			// }
+			// if err := processServer.Send(resp); err != nil {
+			// 	log.Error().Err(err).Msg("Error sending response")
+			// }
+
+		default:
+			log.Warn().Msgf("Unknown request type: %T", value)
+		}
+	}
+}
+
+func main() {
+	port := flag.Int("port", 9001, "gRPC port")
+	write_data_to_file = flag.Bool("write_data_to_file", false, "write data to file")
+	flag.Parse()
+
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatal().Err(err).Msgf("failed to listen: %v", err)
+	}
+
+	new_req_body, err = os.ReadFile("/Users/renuka/Downloads/ext_proc_test/roar.mp4")
+	if err != nil {
+		log.Fatal().Err(err).Msg("Error reading file")
+	}
+	new_req_body_chunks = splitIntoChunks(new_req_body)
+
+	new_resp_body, err = os.ReadFile("/Users/renuka/Downloads/ext_proc_test/something_just_like_this.mp4")
+	if err != nil {
+		log.Fatal().Err(err).Msg("Error reading file")
+	}
+	new_resp_body_chunks = splitIntoChunks(new_resp_body)
+
+	gs := grpc.NewServer(
+		grpc.MaxRecvMsgSize(1024*1024*50), // 50 MB
+		grpc.MaxSendMsgSize(1024*1024*50), // 50 MB
+	)
+	ext_proc_v3.RegisterExternalProcessorServer(gs, &server{})
+	log.Info().Msgf("gRPC server listening on port %d", *port)
+	gs.Serve(lis)
+}
